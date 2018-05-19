6d35e7de6c587b494aa04c76c1048cf76c0bc9ef
201311105 <93kjh1105@daum.net>
2018-05-19 14:36:00 +0900

    socket code 수정

--- Src/Python/client_socket.py
+++ Src/Python/client_socket.py
@@ -1,6 +1,5 @@
 from socket import *
 from threading import *
-import json
 
 class client_socket(object):
 	def __init__(self):
@@ -9,32 +8,42 @@ class client_socket(object):
 		self.udp_sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 		self.addr = gethostbyname(gethostname())
 
+	'''
+		udp socket server를 찾기 위한 broadcast
+		:param
+		subject : server로부터 원하는 데이터 요청하기 위한 문자열 
+	'''
 	def broadcast_verify(self, subject):
 		self.udp_sock.sendto(("Profit_Cut?"+subject).encode(), ('255.255.255.255', 12222))
 		data, addr = self.udp_sock.recvfrom(200)
 		if data.decode() == "Profit_OK":
-			return data, addr
+			return addr
 		else:
 			return False
 
-	def tcp_connect_recv(self, addr):
-		self.tcp_sock.connect((addr, 12223))
-		chain = self.tcp_sock.recv(65535)
-
-		return chain
-
-	def disconnect(self, sock):
-		sock.close()
-
+	'''
+		tcp socket server에 데이터 전송할 것을 요청 
+		:param
+		addrlist : tcp server 들의 주소
+		chainlist : tcp server 로부터 받아온 데이터를 저장할 리스트
+	'''
+	def tcp_recv(self, addrlist, chainlist):
+		for addr in addrlist:
+			try:
+				self.tcp_sock.connect((addr, 12223))
+				chain = self.tcp_sock.recv(65535)
+				chainlist.append(chain)
+			finally:
+				self.tcp_sock.close()
+				self.tcp_sock = socket(AF_INET, SOCK_STREAM)
 
 sock = client_socket()
-data, addr = sock.broadcast_verify()
-print(data, addr)
+addrlist = set()
+chainlist = []
+while len(addrlist) < 2: 
+	addr = sock.broadcast_verify('vote')
+	print(addr[0])
+	addrlist.add(addr[0])
 
-if data:
-	chain = sock.tcp_connect_recv(addr[0])
-	print(chain)
-
-
-else:
-	print(False)
+sock.tcp_recv(addrlist, chainlist)
+print(chainlist)
--- Src/Python/server_socket.py
+++ Src/Python/server_socket.py
@@ -1,16 +1,25 @@
 from socket import *
 from threading import *
-import json
 
 class server_socket(object):
 	def __init__(self):
 		self.tcp_sock = socket(AF_INET, SOCK_STREAM)
 		self.udp_sock = socket(AF_INET, SOCK_DGRAM)
 		self.addr = gethostbyname(gethostname())
-
+	
+	'''
+		udp socket의 주소와 포트 고정
+	'''
 	def udp_bind(self):
 		self.udp_sock.bind(('', 12222))
 	
+	
+	'''
+		client로부터 요청이 들어오면 검증 후 쓰레드를 생성해 데이터 전송
+		:param
+		subject : 요청하는 데이터를 구별하기 위한 문자열
+		info : 전송할 데이터
+	'''
 	def udp_server(self, subject, info):
 		while True:
 			data, c_addr = self.udp_sock.recvfrom(100)
@@ -20,15 +29,24 @@ class server_socket(object):
 				t = Thread(target = self.tcp_send_chain, args = (info,))
 				t.start()
 
+	'''
+		tcp socket의 주소와 포트 고정
+	'''
 	def tcp_bind(self):
 		self.tcp_sock.bind(('', 12223))
 		self.tcp_sock.listen(8)
-		
+	
+	'''
+		client로부터 요청이 들어오면 데이터 전송
+	'''
 	def tcp_send_chain(self, info):	
 		client_sock, addr = self.tcp_sock.accept()
 		client_sock.send(json.dumps(info).encode())		
 
 
+	'''
+		소켓 종료
+	'''
 	def disconnect(self, sock):
 		sock.close()
 
@@ -36,4 +54,4 @@ sock = server_socket()
 sock.udp_bind()
 sock.tcp_bind()
 info = [["chain"], ["checking list"]]
-sock.udp_server(info)
+sock.udp_server('vote', info)
