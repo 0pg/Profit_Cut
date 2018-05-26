7aee727f9a8b72436025449a65088c93e7dbca0f
Lim Hyun (임현) <hyunzion@gmail.com>
2018-05-26 11:38:35 +0900

    Mod LoginActivity.java
    사용자 인증 시 Database 생성

--- Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/LoginActivity.java
+++ Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/LoginActivity.java
@@ -1,11 +1,18 @@
 package profitcut.votechain;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    boolean databaseCreated = false;
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,25 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onButtonAuthentication(View view) {
         Intent ParticipationIntent = new Intent(LoginActivity.this, VoteParticipationActivity.class);
+
+        createDatabase();
+
         startActivity(ParticipationIntent);
+    }
+
+    private void createDatabase() {
+        String name = "profitcut";
+
+        try {
+            db = openOrCreateDatabase(
+                    name,
+                    Activity.MODE_PRIVATE,
+                    null);
+
+            databaseCreated = true;
+            Toast.makeText(getApplicationContext(), "" + name + " Database 생성 ", Toast.LENGTH_LONG). show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "" + name + " Database 생성 오류", Toast.LENGTH_LONG). show();
+        }
     }
 }
