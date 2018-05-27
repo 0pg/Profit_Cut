2137afe9d82e89e5096eb882d861ecc3a638cf6c
201311105 <93kjh1105@daum.net>
2018-05-27 22:13:28 +0900

    error modify

--- Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/TestActivity.java
+++ Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/TestActivity.java
@@ -23,6 +23,7 @@ public class TestActivity extends AppCompatActivity {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         Intent ReturnIntent = new Intent(TestActivity.this, LoginActivity.class);;
         startActivity(ReturnIntent);
     }
@@ -75,6 +76,13 @@ public class TestActivity extends AppCompatActivity {
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         }
 =======
+        startActivity(ReturnIntent);
+>>>>>>> parent of 0a64137... Add SQLiteOpenHelper
+=======
+        String PhoneNum = telManager.getLine1Number();
+
+        Toast.makeText(getApplicationContext(), PhoneNum, Toast.LENGTH_LONG). show();
+
         startActivity(ReturnIntent);
 >>>>>>> parent of 0a64137... Add SQLiteOpenHelper
 =======
