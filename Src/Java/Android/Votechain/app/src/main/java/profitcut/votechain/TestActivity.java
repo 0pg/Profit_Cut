8b2119745e40efa8ca42b424b82ddee4af06eb79
Lim Hyun (임현) <hyunzion@gmail.com>
2018-05-27 22:18:39 +0900

    Revert "Add SQLiteOpenHelper"
    This reverts commit 0a6413781fcc673819c138e1a8691a587a19547c.

---
+++ Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/TestActivity.java
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+=======
+package profitcut.votechain;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Toast;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void onButtonReturn(View view) {
+        Intent ReturnIntent = new Intent(TestActivity.this, LoginActivity.class);
+
+        TelephonyManager telManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        String PhoneNum = telManager.getLine1Number();
+
+        Toast.makeText(getApplicationContext(), PhoneNum, Toast.LENGTH_LONG). show();
+
+        startActivity(ReturnIntent);
+    }
+}
+>>>>>>> parent of 0a64137... Add SQLiteOpenHelper
