7d46cab61adb31f2a636af37b058bc0380864c58
Lim Hyun (임현) <hyunzion@gmail.com>
2018-05-28 20:25:50 +0900

    Add 체크 박스 중복 제거 기능

--- Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/VoteActivity.java
+++ Src/Java/Android/Votechain/app/src/main/java/profitcut/votechain/VoteActivity.java
@@ -4,13 +4,52 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class VoteActivity extends AppCompatActivity {
+
+    CheckBox limBox;
+    CheckBox kimBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        limBox = (CheckBox) findViewById(R.id.limBox);
+        kimBox = (CheckBox) findViewById(R.id.kimBox);
+
+        limBox.setOnClickListener(new CheckBox.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (limBox.isChecked()) {
+                    if (kimBox.isChecked()) {
+                        kimBox.setChecked(false);
+                    }
+                    Toast.makeText(getApplicationContext(), "임현 선택됨", Toast.LENGTH_LONG).show();
+                } else {
+
+                }
+            }
+        });
+
+        kimBox.setOnClickListener(new CheckBox.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (kimBox.isChecked()) {
+                    if (limBox.isChecked()) {
+                        limBox.setChecked(false);
+                    }
+                    Toast.makeText(getApplicationContext(), "자훈 선택됨", Toast.LENGTH_LONG).show();
+                } else {
+
+                }
+            }
+        });
     }
 
     public void onButtonVote(View view) {
